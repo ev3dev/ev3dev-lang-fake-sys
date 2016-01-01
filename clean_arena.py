d39b1180ea3a1553cc9753ae97e01934f4e36ffb
Denis Demidov <dennis.demidov@gmail.com>
2016-01-01 16:21:38 +0300

    Add helper scripts

---
+++ clean_arena.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+import os
+import shutil
+
+def clean_arena():
+    root = os.path.dirname(os.path.realpath(__file__))
+    shutil.rmtree(os.path.join(root, 'arena'))
+
+if __name__ == '__main__':
+    clean_arena()
---
+++ populate_arena.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+
+import os
+import shutil
+
+# Where to put each device in the arena
+class_path = {
+        'infrared_sensor' : 'lego-sensor/sensor{0}',
+        'touch_sensor'    : 'lego-sensor/sensor{0}',
+        'medium_motor'    : 'tacho-motor/motor{0}'
+        }
+
+def populate_arena(devices):
+    """
+    Copies specified devices to arena.
+    `devices` is a dictionary mapping device name to a tuple of
+    device index/device address.
+    """
+
+    for dev,prm in devices.items():
+        assert dev in class_path, 'Unregistered device requested'
+
+        root = os.path.dirname(os.path.realpath(__file__))
+        src = os.path.join(root, 'devices', dev)
+        dst = os.path.join(root, 'arena', class_path[dev].format(prm[0]))
+
+        shutil.copytree(src, dst)
+
+        if len(prm) > 1:
+            with open(os.path.join(dst, 'address'), 'w') as address:
+                address.write('{0}\n'.format(prm[1]))
+
+if __name__ == '__main__':
+    import sys
+
+    if len(sys.argv[1:]) < 1:
+        print('Usage: {0} <device>:<index>[@<address>] ...'.format(sys.argv[0]))
+        sys.exit(1)
+
+    devices = {}
+    for arg in sys.argv[1:]:
+        dev_idx = arg.split(':')
+        assert len(dev_idx) == 2, 'Incorrect command line parameter: {0}'.format(arg)
+        devices[dev_idx[0]] = dev_idx[1].split('@')
+
+    populate_arena(devices)
