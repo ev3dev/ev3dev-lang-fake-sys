34e140e99795425fe9fc011fbff54369348bf790
Wasabi Fan <wasabifan@outlook.com>
2016-01-03 00:23:27 -0800

    Don't throw errors when there is nothing to clean

--- clean_arena.py
+++ clean_arena.py
@@ -5,7 +5,7 @@ import shutil
 
 def clean_arena():
     root = os.path.dirname(os.path.realpath(__file__))
-    shutil.rmtree(os.path.join(root, 'arena'))
+    shutil.rmtree(os.path.join(root, 'arena'), ignore_errors=True)
 
 if __name__ == '__main__':
     clean_arena()
