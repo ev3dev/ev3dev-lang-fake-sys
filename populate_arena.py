13904dc7d0555857d4a0a85a59252e9a0812cfd6
Kaelin Laundry <wasabifan@outlook.com>
2018-06-28 20:21:24 -0700

    Add large motor and support multiple devices of the same type (#12)
    * Raw dump of new large motor from device (needs to be modified)
    
    * Clean up new large motor data
    
    * Register new large motor in script
    
    * Refactor fake-sys script to enable multiple devices of the same type

--- populate_arena.py
+++ populate_arena.py
@@ -7,14 +7,15 @@ import shutil
 class_path = {
         'infrared_sensor' : 'lego-sensor/sensor{0}',
         'touch_sensor'    : 'lego-sensor/sensor{0}',
-        'medium_motor'    : 'tacho-motor/motor{0}'
+        'medium_motor'    : 'tacho-motor/motor{0}',
+        'large_motor'    : 'tacho-motor/motor{0}'
         }
 
 def populate_arena(devices):
     """
     Copies specified devices to arena.
-    `devices` is a dictionary mapping device name to a tuple of
-    device index/device address.
+    `devices` is an array of tuples containing device types, indices and
+    addresses.
     """
 
     root = os.path.dirname(os.path.realpath(__file__))
@@ -23,19 +24,18 @@ def populate_arena(devices):
 
     shutil.copytree(src, dst)
 
-    for dev,prm in devices.items():
-        assert dev in class_path, 'Unregistered device "{0}" requested'.format(dev)
+    for dev_type, index, address in devices:
+        assert dev_type in class_path, 'Unregistered device "{0}" requested'.format(dev_type)
 
-        src = os.path.join(root, 'devices', dev)
-        dst = os.path.join(root, 'arena', class_path[dev].format(prm[0]))
+        src = os.path.join(root, 'devices', dev_type)
+        dst = os.path.join(root, 'arena', class_path[dev_type].format(index))
 
         shutil.copytree(src, dst)
 
-        print("\t{0}\t{1}\t{2}".format(prm[0], prm[1], dst))
+        print("\t{0}\t{1}\t{2}".format(index, address, dst))
 
-        if len(prm) > 1:
-            with open(os.path.join(dst, 'address'), 'w') as address:
-                address.write('{0}\n'.format(prm[1]))
+        with open(os.path.join(dst, 'address'), 'w') as address_file:
+            address_file.write('{0}\n'.format(address))
 
 if __name__ == '__main__':
     import sys
@@ -44,10 +44,11 @@ if __name__ == '__main__':
         print('Usage: {0} <device>:<index>@<address> ...'.format(sys.argv[0]))
         sys.exit(1)
 
-    devices = {}
+    devices = []
     for arg in sys.argv[1:]:
         dev_idx = arg.split(':', 1)
         assert len(dev_idx) == 2, 'Incorrect command line parameter: {0}'.format(arg)
-        devices[dev_idx[0]] = dev_idx[1].split('@')
+        location_info = dev_idx[1].split('@')
+        devices.append( (dev_idx[0], location_info[0], location_info[1]) )
 
     populate_arena(devices)
